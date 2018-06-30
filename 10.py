793f2ce320db41daa25d82e9039b9458e790b5c6
Ben <legoben1998@gmail.com>
2018-06-29 23:50:43 -0700

    10

--- 10.py
+++ 10.py
@@ -1,9 +1,8 @@
 # Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
 
-import bisect
+from collections import deque
 import time
-times = []
-store = {}
+store = deque() # linked list
 
 def hello():
     print("hello1")
@@ -12,18 +11,26 @@ def hello2():
     print("bye")
 
 def add_job(f, n):
-    if n not in store:
-        bisect.insort(times, n)
-        store[n] = []
+    i = 0
 
-    store[n].append(f)
+    for t in store:
+        if t['time'] == n:
+            t['jobs'].append(f)
+            break
+        elif t['time'] > n:
+            store.insert(i, {"time": n, "jobs": [f]})
+            break
+
+        i += 1
+    else:
+        store.insert(i, {"time": n, "jobs": [f]})
 
 def run_scheduler():
     start = int(round(time.time() * 1000))
-    while len(times) > 0:
-        if times[0] <= int(round(time.time() * 1000)) - start:
-            t = times.pop(0)
-            [x() for x in store[t]]
+    while len(store) > 0:
+        if store[0]['time'] <= int(round(time.time() * 1000)) - start:
+            t = store.popleft()
+            [x() for x in t['jobs']]
 
 if __name__ == "__main__":
     add_job(hello, 400)
@@ -32,6 +39,6 @@ if __name__ == "__main__":
     add_job(hello2, 5000)
     add_job(hello, 5000)
 
-    run_scheduler()
+    run_scheduler() # In theory, this could be run in diff thread
     pass
 
